be91e003c1365340c8c041123eb2616bb21a893d
GeorgeFarg <georgefarg7@gmail.com>
2026-02-08 11:21:59 +0200

    feat(home): restructure Home component layout and add new content sections

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,25 +1,41 @@
 import Image, { type ImageProps } from "next/image";
-import { Button } from "@repo/ui/button";
-import styles from "./page.module.css";
-
-type Props = Omit<ImageProps, "src"> & {
-  srcLight: string;
-  srcDark: string;
-};
-
-const ThemeImage = (props: Props) => {
-  const { srcLight, srcDark, ...rest } = props;
-
-  return (
-    <>
-      <Image {...rest} src={srcLight} className="imgLight" />
-      <Image {...rest} src={srcDark} className="imgDark" />
-    </>
-  );
-};
 
 export default function Home() {
   return (
-    <h1 className="">hello world</h1>
+    <div className="h-full gradient-bg border-b-2 border-b-blue-950">
+      <div className="h-full flex justify-around items-center">
+        <div className="text-main">
+          {/* The new sections */}
+          <div className="inline-flex w-fit items-center gap-2 rounded-full border border-action-blue/30 bg-action-blue/10 px-3 py-1 text-xs font-semibold text-action-blue">
+            <span className="relative flex h-2 w-2">
+              <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-main opacity-75"></span>
+              <span className="relative inline-flex rounded-full h-2 w-2 bg-main"></span>
+            </span>
+            New: Real-time video and chat
+          </div>
+
+          <div className="mt-3">
+            <h1 className="text-white text-8xl font-bold">Learn.</h1>
+            <h1 className="text-8xl font-bold bg-linear-to-r from-blue-600 to-violet-500 bg-clip-text text-transparent">
+              Collaborate.
+            </h1>
+            <h1 className="text-white text-8xl font-bold">Succeed.</h1>
+
+            <p className="text-slate-400 mt-5 max-w-100">
+              Modern problems require modern solutions and for that. Learning
+              Managements System designed to unify compus leanring, drive
+              students to success, and simplify administration
+            </p>
+          </div>
+        </div>
+        <Image
+          src={"/landing.webp"}
+          width={600}
+          height={600}
+          alt="hero section"
+          className="aspect-video object-cover h-96 rounded-xl shadow-lg shadow-main"
+        />
+      </div>
+    </div>
   );
 }
